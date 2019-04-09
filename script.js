158c053de305bb53d3839774c37ac1cae334a358
ekaterinagushan <43372441+ekaterinagushan@users.noreply.github.com>
2019-04-09 12:06:26 +0300

    js first steps

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+const form = document.querySelector('form')
+
+function startTimer(e) {
+  e.preventDefault()
+
+  const input = e.target.querySelector('input')
+  console.log('event: ', input)
+}
+
+form.onsubmit = startTimer
