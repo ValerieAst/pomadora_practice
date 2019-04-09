4710fd18476966cddb257927cf9beb0e74a36def
ekaterinagushan <43372441+ekaterinagushan@users.noreply.github.com>
2019-04-09 12:57:37 +0300

    JS progress

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 
 const form = document.querySelector('form')
+const tasks = document.querySelector('ul')
+const progress = document.querySelector('.progress')
+const WORK_TIME = 2000
 
 function startTimer(e) {
   e.preventDefault()
 
   const input = e.target.querySelector('input')
-  console.log('event: ', input)
+
+  input.disabled = true
+  progress.style.width = '100vw'
+  progress.style.transitionDuration = WORK_TIME + 'ms'
+
+  setTimeout(function () {
+    input.disabled = false
+    
+    progress.style.transitionDuration = ''
+    progress.style.width = '0'
+    // listItem === li
+    const listItem = document.createElement('li')
+
+    listItem.innerText = input.value
+    // Добавляем внутрь ul новый элемент
+    tasks.appendChild(listItem)
+
+
+    input.value = ''
+
+  }, WORK_TIME)
 }
 
 form.onsubmit = startTimer
